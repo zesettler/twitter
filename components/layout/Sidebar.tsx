ec8a2a6680bbf6cad7062515cfc0fbaa678f588d
zesettler <gromovgrin@gmail.com>
2023-06-17 22:42:11 +0300

    layout

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,47 @@
+import { BiLogOut } from 'react-icons/bi';
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+
+
+import SidebarItem from './SidebarItem';
+import SidebarLogo from './SidebarLogo';
+
+const Sidebar = () => {
+
+  const items = [
+    {
+      icon: BsHouseFill,
+      label: 'Home',
+      href: '/',
+    },
+    {
+      icon: BsBellFill,
+      label: 'Notifications',
+      href: '/notifications',
+    },
+    {
+      icon: FaUser,
+      label: 'Profile',
+    },
+  ]
+
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+        <div className="flex flex-col items-end">
+          <div className="space-y-2 lg:w-[230px]">
+            <SidebarLogo />
+            {items.map((item) => (
+              <SidebarItem
+                key={item.href}
+                href={item.href} 
+                icon={item.icon} 
+                label={item.label}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+  )
+};
+
+export default Sidebar;
