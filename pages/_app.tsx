92196a9cc550ccc26248d00a2ea24663a221c836
zesettler <gromovgrin@gmail.com>
2023-06-19 22:12:27 +0300

    login modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app'
 
 import Layout from '@/components/Layout'
-import Modal from '@/components/Modal'
 import '@/styles/globals.css'
+import LoginModal from '@/components/modals/LoginModal'
+import RegisterModal from '@/components/modals/RegisterModal'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    <RegisterModal />
+      <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
