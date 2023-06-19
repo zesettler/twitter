5d2562637792bb2f7a3ac3e14b223aedfda7656c
zesettler <gromovgrin@gmail.com>
2023-06-19 21:00:25 +0300

    input component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import Layout from '@/components/Layout'
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+
+import Layout from '@/components/Layout'
+import Modal from '@/components/Modal'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
